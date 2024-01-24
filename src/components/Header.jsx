e624e92e68f1143af581808e73592b6d9e40d170
Ramadani-coding <ramacoding9@gmail.com>
2024-01-25 00:38:58 +0800

    fix: header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,14 +13,14 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-[#E5E5E5]">
+    <header className="bg-amber-900">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-between items-center h-20">
           <div>
-            <h1 className="text-[#fb923c] text-2xl md:text-3xl font-bold">
+            <h1 className="text-orange-300 text-2xl md:text-3xl font-bold">
               NYUNFOOD
             </h1>
-            <small className="text-[#444444] text-xs md:text-[14px]">
+            <small className="text-orange-300 text-xs md:text-[14px]">
               Savor Joyful Flavor Advanture!
             </small>
           </div>
@@ -32,8 +32,8 @@ const Header = () => {
                     key={index}
                     className={
                       router === menu.path
-                        ? "text-[#fb923c] hover:text-[#fb923c] cursor-pointer"
-                        : "text-[#444444] hover:text-[#fb923c] transition-all cursor-pointer"
+                        ? "text-orange-300 hover:text-orange-300 cursor-pointer"
+                        : "text-orange-100 hover:text-[#fb923c] transition-all cursor-pointer"
                     }
                     onClick={() => handleMovePage(menu.path)}
                   >
