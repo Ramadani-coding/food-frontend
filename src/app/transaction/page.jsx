29d7dfa2fcbf952ce1414ad4e3a6010235fcb50c
NowFall <m.naufal.baidhawi@gmail.com>
2024-02-23 08:57:28 +0800

    update:
     Page Transaction

--- src/app/transaction/page.jsx
+++ src/app/transaction/page.jsx
@@ -3,13 +3,143 @@ import Layouts from "@/components/Layouts";
 const Page = () => {
   return (
     <Layouts>
-      <div className="w-full h-screen flex justify-center items-center flex-col">
-        <h1>INI HALAMAN TRANSAKSI</h1>
-        <h1>MASIH DALAM TAHAP DEVELOP</h1>
-        <p>
-          Jika Anda menggunkan handphone harap nyalakan mode desktop untuk
-          mengakses fitur terbaru
-        </p>
+      <div className="w-full h-screen flex justify-center">
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Transaction Number</h2>
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Name Food</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="hover">
+                    <td>Sandwich</td>
+                    <td>Rp.10.000</td>
+                    <td>5</td>
+                  </tr>
+                  <tr className="hover">
+                    <td>Hot Coffee</td>
+                    <td>Rp.5.000</td>
+                    <td>2</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <ul className="menu bg-base-100 w-56 rounded-box">
+              <li>
+                <a>Total = Rp.60.000</a>
+              </li>
+            </ul>
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Transaction Number</h2>
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Name Food</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="hover">
+                    <td>Sandwich</td>
+                    <td>Rp.10.000</td>
+                    <td>5</td>
+                  </tr>
+                  <tr className="hover">
+                    <td>Hot Coffee</td>
+                    <td>Rp.5.000</td>
+                    <td>2</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <ul className="menu bg-base-100 w-56 rounded-box">
+              <li>
+                <a>Total = Rp.60.000</a>
+              </li>
+            </ul>
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Transaction Number</h2>
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Name Food</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="hover">
+                    <td>Sandwich</td>
+                    <td>Rp.10.000</td>
+                    <td>5</td>
+                  </tr>
+                  <tr className="hover">
+                    <td>Hot Coffee</td>
+                    <td>Rp.5.000</td>
+                    <td>2</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <ul className="menu bg-base-100 w-56 rounded-box">
+              <li>
+                <a>Total = Rp.60.000</a>
+              </li>
+            </ul>
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
+        <div className="card w-96 bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Transaction Number</h2>
+            <div className="overflow-x-auto">
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Name Food</th>
+                    <th>Price</th>
+                    <th>Quantity</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr className="hover">
+                    <td>Sandwich</td>
+                    <td>Rp.10.000</td>
+                    <td>5</td>
+                  </tr>
+                  <tr className="hover">
+                    <td>Hot Coffee</td>
+                    <td>Rp.5.000</td>
+                    <td>2</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <ul className="menu bg-base-100 w-56 rounded-box">
+              <li>
+                <a>Total = Rp.60.000</a>
+              </li>
+            </ul>
+            <div className="card-actions justify-end"></div>
+          </div>
+        </div>
       </div>
     </Layouts>
   );
